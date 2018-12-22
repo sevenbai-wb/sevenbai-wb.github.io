d409f90fc8c67c13e75a3366692165e05d7fd851
seven <sevenbai@kingkit101.com>
2018-12-22 17:52:12 +0800

    updated imageml block string

--- webduino-module-imageml/blockly/msg/blocks/en.js
+++ webduino-module-imageml/blockly/msg/blocks/en.js
@@ -1,6 +1,6 @@
 Blockly.Msg.WEBDUINO_IMAGEML_CAMERA_SRC = "Camera Source:";
 Blockly.Msg.WEBDUINO_IMAGEML_CAMERA_LOCAL = "Local";
-Blockly.Msg.WEBDUINO_IMAGEML_MODEL_NAME = ", model name:";
+Blockly.Msg.WEBDUINO_IMAGEML_MODEL_NAME = ", load model:";
 Blockly.Msg.WEBDUINO_IMAGEML_CLASSIFY = "Start to classify, when the class index is";
 Blockly.Msg.WEBDUINO_IMAGEML_CLASSIFY2 = "";
 Blockly.Msg.WEBDUINO_IMAGEML_RUN = "Run";
--- webduino-module-imageml/blockly/msg/blocks/zh-hant.js
+++ webduino-module-imageml/blockly/msg/blocks/zh-hant.js
@@ -1,6 +1,6 @@
 Blockly.Msg.WEBDUINO_IMAGEML_CAMERA_SRC = "相機位置：";
 Blockly.Msg.WEBDUINO_IMAGEML_CAMERA_LOCAL = "本機";
-Blockly.Msg.WEBDUINO_IMAGEML_MODEL_NAME = "，模型名稱：";
+Blockly.Msg.WEBDUINO_IMAGEML_MODEL_NAME = "，載入模型：";
 Blockly.Msg.WEBDUINO_IMAGEML_CLASSIFY = "對影像進行分類，當分類編號為";
 Blockly.Msg.WEBDUINO_IMAGEML_CLASSIFY2 = "時";
 Blockly.Msg.WEBDUINO_IMAGEML_RUN = "執行";
