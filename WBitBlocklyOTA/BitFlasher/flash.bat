280510036da322a5b86668856937ec43b6104431
seven <sevenbai@kingkit101.com>
2018-12-27 16:03:26 +0800

    updated esptool

--- WBitBlocklyOTA/BitFlasher/flash.bat
+++ WBitBlocklyOTA/BitFlasher/flash.bat
@@ -1,1 +1,1 @@
-esptool.exe -p %1 -b %2 write_flash -z -fm dio -ff 40m -fs detect 0x1000 bootloader_dio_40m.bin 0x8000 partitions.bin 0xe000 boot_app0.bin 0x10000 firmware.bin
+esptool.exe -p %1 -b %2 write_flash -z -fm dio -ff 40m -fs detect 0x1000 bootloader_dio_40m.bin 0x8000 partitions.bin 0xe000 boot_app0.bin 0x10000 bit_default.bin
